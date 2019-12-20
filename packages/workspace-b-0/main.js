4a77c568e338772f0ecceefef58b0a5b76381aca
Brian Takita <brian.takita@gmail.com>
2019-12-20 12:20:05 -0500

    comments on bug https://youtrack.jetbrains.com/issue/WEB-41863 reproduction

--- packages/workspace-b-0/main.js
+++ packages/workspace-b-0/main.js
@@ -1,2 +1,4 @@
+// GOTO definition on const { HELLO } navigates to ./app.ts
 const { HELLO } = require('./app')
+// GOTO definition on console.debug(HELLO) navigates to ./app.d.ts
 console.debug(HELLO)
